cd08921f280503af65c00c9c8734e19b77699b80
Georgi Rashkov <99902659+rashkov7@users.noreply.github.com>
2024-06-26 18:15:51 +0300

    Update header.test.js

--- tests/ui/header.test.js
+++ tests/ui/header.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Check header', async ({ page }) => {
     await page.goto('http://localhost:8090'); 
-    const homeLink = await page.$('a[href=/]');
+    const homeLink = await page.$('a');
     const text = await homeLink.textContent();
     expect(text).toBe('Home');
   });
